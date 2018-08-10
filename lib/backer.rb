a1420a212c635b03edbbd23fcbf226c8c55d487d
Juan Tenezaca <tenj34@gmail.com>
2018-08-10 16:15:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,6 @@
 class Backer
   attr_accessor :name, :backed_projects
 
-
-
   def initialize(name)
     @name = name
     @backed_projects = []
@@ -10,6 +8,7 @@ class Backer
 
   def back_project(project) # accepts a project arguement
     @backed_projects << project # stores project in a backend_projects array
+    @backed_projects << @backer
   end
 
 
