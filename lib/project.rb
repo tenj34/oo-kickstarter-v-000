e144c103125d33218107f676c6249c0312f23ade
Juan Tenezaca <tenj34@gmail.com>
2018-08-10 16:17:28 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,6 @@ class Project
 
   def add_backer(backer) #accepts a backer as an arguement
     @backers << backer  # stores in a backers array
-    project.backers << self# also adds the backer to the projects backer array
 
   end
 
